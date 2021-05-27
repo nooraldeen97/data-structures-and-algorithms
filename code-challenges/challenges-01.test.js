2e17659f9a8ec169280d06a1c2ee2ecd92e4a04b
nooraldeen97 <noor-aldeen97@hotmail.com>
2021-05-27 15:00:18 +0300

    finished all challenges

--- code-challenges/challenges-01.test.js
+++ code-challenges/challenges-01.test.js
@@ -140,6 +140,13 @@ This function should use forEach to populate your grocery list based on the stor
 
 const createList = (availableItems) => {
   // Solution code here...
+  let newList=[];
+  availableItems.forEach((item)=> {
+    if (item.available == true) {
+      newList.push(item.name)
+    }
+  })
+  return newList;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -158,6 +165,19 @@ Return the resulting output array.
 
 const fizzbuzz = (arr) => {
   // Solution code here...
+  let newNumbers=[];
+  arr.forEach((value)=> {
+    if (value%3 == 0 && value%5 == 0) {
+      newNumbers.push('Fizz Buzz')
+    }else if (value%5 == 0 ) {
+      newNumbers.push('Buzz')
+    }else if (value%3 == 0) {
+      newNumbers.push('Fizz')
+    } else{
+      newNumbers.push(value)
+    }
+  })
+  return newNumbers ;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -211,7 +231,7 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   const inputs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
   test('It should print out messages or numbers', () => {
